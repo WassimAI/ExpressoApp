c56a8d69b08241329493764532a5a65937f144e5
wassimAI - Wassim Ibrahim <40058827+WassimInstaclass@users.noreply.github.com>
2019-09-30 10:58:23 +0400

    Done with Info Page - next is bug fixing

--- Expresso/Expresso/Pages/InfoPage.xaml.cs
+++ Expresso/Expresso/Pages/InfoPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,31 @@ namespace Expresso.Pages
         public InfoPage()
         {
             InitializeComponent();
+        }
+
+        private void TapFacebook_Tapped(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("https://www.facebook.com/instaclassme"));
+        }
+
+        private void TabInstagram_Tapped(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("https://www.instagram.com/instaclassme"));
+        }
+
+        private void TapTwitter_Tapped(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("https://www.twitter.com/instaclassme"));
+        }
+
+        private void TapYoutube_Tapped(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("https://www.youtube.com/channel/UCmb7mFxNtEeIPWaej2ZNy9g"));
+        }
+
+        private void TapCall_Tapped(object sender, EventArgs e)
+        {
+            PhoneDialer.Open("00971501417553");
         }
     }
 }
